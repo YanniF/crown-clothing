63d1db9336667b6c5b833c7f6009a84b0c69e7d0
YanniF <yanni_fernanda@hotmail.com>
2020-06-09 00:13:48 +0200

    Collection preview component

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './homepage.scss';
-import Directory from '../components/Directory/Directory';
+import Directory from '../../components/Directory/Directory';
 
 const HomePage = () => (
 	<div className="homepage">
