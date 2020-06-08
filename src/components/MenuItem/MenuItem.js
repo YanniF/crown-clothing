e666e6cb4a672b5bb7f7ed0b87975ec101f25f13
YanniF <yanni_fernanda@hotmail.com>
2020-06-08 22:15:02 +0200

    Added routing

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menuItem.scss';
 
-const MenuItem = ({ section }) => {
+const MenuItem = ({ section, history, match }) => {
 	return (
-		<div className={`menu-item ${section.size || ''}`}>
+		<div className={`menu-item ${section.size || ''}`} onClick={() => history.push(`${match.url}${section.linkUrl}`)}>
 			<div className="background-image" style={{ backgroundImage: `url(${section.imageUrl})` }} />
 			<div className="content">
 				<h2 className="title">{section.title.toUpperCase()}</h2>
@@ -14,4 +15,4 @@ const MenuItem = ({ section }) => {
 	);
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
