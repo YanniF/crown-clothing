e2150185552b737aa475bcf597fbeeb5a4f77f78
Yanni Fraga <yanni@innoscripta.com>
2020-06-10 10:49:38 +0200

    Custom buttom component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,40 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import './signIn.scss'
+import "./signIn.scss";
+import FormInput from "../FormInput/FormInput";
+import CustomButton from "../CustomButton/CustomButton";
 
 class SignIn extends Component {
   state = {
-    email: '',
-    password: ''
-  }
+    email: "",
+    password: "",
+  };
 
-  handleSubmit = e => {
-    e.preventDefault()
-    this.setState({ email: '', password: '' })
-  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
 
   handleChange = ({ target }) => {
-    const { name, value } = target
-    this.setState({ [name]: value }, () => console.log(this.state))
-  }
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => console.log(this.state));
+  };
 
   render() {
-    const { email, password } = this.state
+    const { email, password } = this.state;
 
     return (
-      <div className='sign-in'>
+      <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input type="email" name="email" value={email} onChange={(e) => this.handleChange(e)} required />
-          <label>Email</label>
-          <input type="password" name="password" value={password} onChange={this.handleChange} required />
-          <label>Password</label>
-          <input type="submit" value="Submit"/>
+          <FormInput
+            type="email"
+            name="email"
+            label="Email"
+            value={email}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            label="Password"
+            value={password}
+            handleChange={this.handleChange}
+            required
+          />
+          <CustomButton type='submit'>Sign In</CustomButton>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default SignIn
+export default SignIn;
